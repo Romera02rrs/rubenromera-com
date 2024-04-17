fda82cf126652bcc38f5e710ca14f6b51a6fd853
Romera02rrs <romera02rrs@gmail.com>
2024-04-17 20:07:18 +0200

    Nuevos datos insertados

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,18 +3,18 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'SRLEOM',
+	author: 'Romera02rrs',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'astro-theme-resume',
+	title: 'rubenromera.com',
 	// Meta property used as the default description meta property
-	description: 'The official Astro Resume Theme',
+	description: 'El perfil de Rubén Romera',
 	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: 'en-GB',
+	lang: 'es',
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: 'en_GB',
+	ogLocale: 'es',
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: 'en-GB',
+		locale: 'es',
 		options: {
 			day: 'numeric',
 			month: 'short',
